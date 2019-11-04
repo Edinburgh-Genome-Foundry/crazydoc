a87bfecb9f619981a3181ef9cc35cceeac622ace
Zulko <valentin.zulkower@gmail.com>
2019-11-04 20:28:41 +0000

    Manifest fix

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('crazydoc/version.py').read()) # loads __version__
+exec(open("crazydoc/version.py").read())  # loads __version__
 
 setup(
-    name='crazydoc',
+    name="crazydoc",
     version=__version__,
-    author='Zulko',
-    description='Read genetic sequences from stylized docx files',
-    long_description=open('pypi-readme.rst').read(),
-    license='MIT',
+    author="Zulko",
+    url="https://github.com/Edinburgh-Genome-Foundry/crazydoc",
+    description="Read genetic sequences from stylized docx files",
+    long_description=open("pypi-readme.rst").read(),
+    license="MIT",
     keywords="dna-sequence bioinformatics systems-biology docx",
-    packages=find_packages(exclude='docs'),
-    install_requires=['Biopython', 'python-docx'])
+    packages=find_packages(exclude="docs"),
+    install_requires=["Biopython", "python-docx"],
+)
